21a29d8e0f9d6c80d7c60f3e45da77eafe415ceb
takuyan <fanciful.dream@gmail.com>
2012-09-19 20:36:28 +0900

    add spec

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -4,8 +4,22 @@ describe MainController do
 
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index'
+      get :index
       response.should be_success
+    end
+  end
+
+  describe "GET 'show'" do
+    it "returns http success" do
+      get :show, code: 'hoge'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'search'" do
+    it "returns http success" do
+      get :search, code: 'hoge'
+      response.should redirect_to(code_path(code: 'hoge'))
     end
   end
 
